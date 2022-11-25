4adc59e07f8a3685b438b5614f7ac0cd1de0814e
Adesipe Adeola <princeadeola234@gmail.com>
2022-11-25 14:46:59 -0800

    committed

---
+++ app/src/main/java/com/example/babybuy/HaveAnAccount.java
@@ -0,0 +1,14 @@
+package com.example.babybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HaveAnAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_have_an_account);
+    }
+}
--- app/src/main/java/com/example/babybuy/MainActivity.java
+++ app/src/main/java/com/example/babybuy/MainActivity.java
@@ -1,14 +1,44 @@
 package com.example.babybuy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    CardView individualCard, familyCard, guestCard;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        individualCard = findViewById(R.id.individualCardID);
+        familyCard = findViewById(R.id.familyCardID);
+        guestCard = findViewById(R.id.guestCardID);
+
+        individualCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Individual Card clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        familyCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Family Card clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        guestCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Guest Card clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
