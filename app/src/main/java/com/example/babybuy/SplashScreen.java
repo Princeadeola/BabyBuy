67c3a3569447cd445782616ca478a4905bd07c66
Adesipe Adeola <princeadeola234@gmail.com>
2022-11-28 23:03:51 -0800

    committed

--- app/src/main/java/com/example/babybuy/SplashScreen.java
+++ app/src/main/java/com/example/babybuy/SplashScreen.java
@@ -2,13 +2,28 @@ package com.example.babybuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class SplashScreen extends AppCompatActivity {
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, SplashScreen01.class);
+                startActivity(intent);
+            }
+        }, 2000);
+
     }
 }
