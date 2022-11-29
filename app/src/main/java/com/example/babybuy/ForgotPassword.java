f1ac17cd9f8a435f5f2ca35d3c842d3fbcb5a807
Adesipe Adeola <princeadeola234@gmail.com>
2022-11-29 12:32:47 -0800

    committed

--- app/src/main/java/com/example/babybuy/ForgotPassword.java
+++ app/src/main/java/com/example/babybuy/ForgotPassword.java
@@ -2,13 +2,35 @@ package com.example.babybuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class ForgotPassword extends AppCompatActivity {
+    TextView backToLoginTxt, createAccountTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        backToLoginTxt = findViewById(R.id.backToLoginID);
+        createAccountTxt = findViewById(R.id.createAccountFromForgotPasswordTxtID);
+        backToLoginTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        createAccountTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ForgotPassword.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/babybuy/LoginActivity.java
+++ app/src/main/java/com/example/babybuy/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.babybuy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView forgotPasswordTxt, createAccountTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activty);
+
+        forgotPasswordTxt = findViewById(R.id.forgotPasswordID);
+        createAccountTxt = findViewById(R.id.createAccountFromLoginTxtID);
+
+        forgotPasswordTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
+
+        createAccountTxt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, CreateAccount.class);
+                startActivity(intent);
+            }
+        });
     }
 }
