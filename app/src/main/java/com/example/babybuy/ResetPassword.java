1c40a64aeb378d836b910ca94536baa59120b27a
Adesipe Adeola <princeadeola234@gmail.com>
2022-12-06 14:13:43 -0800

    committed

---
+++ app/src/main/java/com/example/babybuy/ResetPassword.java
@@ -0,0 +1,14 @@
+package com.example.babybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
