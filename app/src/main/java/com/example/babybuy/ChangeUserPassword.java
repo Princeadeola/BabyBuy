e130aad414e863ba6e99ab602b70ee505a505ff2
Adesipe Adeola <princeadeola234@gmail.com>
2022-12-06 17:24:37 -0800

    committed

---
+++ app/src/main/java/com/example/babybuy/ChangeUserPassword.java
@@ -0,0 +1,14 @@
+package com.example.babybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeUserPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_user_password);
+    }
+}
