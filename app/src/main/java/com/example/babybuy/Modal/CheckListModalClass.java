bd430cc125fede3c55afb3f4febc7388fd6e2658
Adesipe Adeola <princeadeola234@gmail.com>
2022-12-13 01:26:57 -0800

    committed

--- app/src/main/java/com/example/babybuy/Modal/CheckListModalClass.java
+++ app/src/main/java/com/example/babybuy/Modal/CheckListModalClass.java
@@ -1,5 +1,25 @@
 package com.example.babybuy.Modal;
 
 public class CheckListModalClass {
-    
+    private int itemImage;
+    private String itemName;
+    private int addItemImage;
+
+    public CheckListModalClass(int itemImage, String itemName, int addItemImage) {
+        this.itemImage = itemImage;
+        this.itemName = itemName;
+        this.addItemImage = addItemImage;
+    }
+
+    public int getItemImage() {
+        return itemImage;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getAddItemImage() {
+        return addItemImage;
+    }
 }
