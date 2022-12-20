4259938c556a40ce513be0ae28519a46a613e748
Adesipe Adeola <princeadeola234@gmail.com>
2022-12-19 22:35:31 -0800

    committed

---
+++ app/src/main/java/com/example/babybuy/NewListActivity.java
@@ -0,0 +1,14 @@
+package com.example.babybuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_list);
+    }
+}
